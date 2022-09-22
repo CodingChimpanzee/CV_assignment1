0e5937289bf5aa265f77c7f5d9dc4c18548d45ea
CodingChimpanzee <kanghyun51015@gm.gist.ac.kr>
2022-09-22 04:17:14 +0000

    prob 2 finished

---
+++ Analysis/PSNR_test_prob2.py
@@ -0,0 +1,138 @@
+# Computer vision programming assignment 1
+# This code is written by
+# 20175003 Sunghyun Kang
+
+# PSNR Analysis for this HW
+
+import numpy as np
+import cv2
+
+# This is for progress bar
+from tqdm import tqdm
+
+# This is for plotting
+import matplotlib.pyplot as plt
+
+#---------------------------------------------------------------------------------#
+
+# MSE, PSNR definition
+def MSE(image_gt, image_h, width, height):
+    difference_square = np.sum(np.square(image_gt-image_h))
+    return np.divide(difference_square, height*width)
+
+def PSNR(image_gt, mse):
+    r = np.amax(image_gt)
+    return 10*np.log10(np.divide(r**2, mse))
+
+#---------------------------------------------------------------------------------#
+
+# import upsampled image as greyscale (I_h0)
+I_h = cv2.imread('/home/Computer_Vision_PA1/upsampled.png')
+I_h = cv2.cvtColor(I_h, cv2.COLOR_BGR2GRAY)
+# Change the form into float type
+I_h = np.array(I_h, dtype = float)
+
+# find the height and width of the image
+height, width = I_h.shape
+
+# import the ground_truth image
+I_gt = cv2.imread('/home/Computer_Vision_PA1/HR.png')
+I_gt = cv2.cvtColor(I_gt, cv2.COLOR_BGR2GRAY)
+# Change the form into float type
+I_gt = np.array(I_gt, dtype = float)
+
+# define low resolution image input
+# which refers to bilinear downsampling(I_h)
+I_l = cv2.resize(I_gt, (height//4, width//4), interpolation = cv2.INTER_LINEAR)
+
+#---------------------------------------------------------------------------------#
+
+# Prior part, please be careful with normalization part
+# 1. define hyperparameter gamma and get gradient of I_h
+Gamma = 6
+x_dir = cv2.Sobel(I_h, -1, dx = 1, dy = 0)
+y_dir = cv2.Sobel(I_h, -1, dx = 0, dy = 1)
+un_normalized_val = np.abs(x_dir) + np.abs(y_dir)
+# Normalize
+maxpoint = np.max(I_h)
+minpoint = np.min(I_h)
+G_h0 = np.divide(np.subtract(un_normalized_val, minpoint), np.subtract(maxpoint, minpoint)) + 1e-10
+
+# 2. get Laplacian of I_h
+un_normalized_laplacian = cv2.Laplacian(I_h, -1)
+# Normalize
+maxpoint = np.max(un_normalized_laplacian)
+minpoint = np.min(un_normalized_laplacian)
+laplacian = np.divide(np.subtract(un_normalized_laplacian, minpoint), np.subtract(maxpoint, minpoint))
+# laplacian = cv2.normalize(un_normalized_laplacian, None, 0, 1, cv2.NORM_MINMAX)
+
+# 3. get the sharp edge
+G_t = G_h0 - laplacian
+G_t = np.clip(G_t, a_min = 0.0, a_max = 1.0)
+
+# 4. Now get the gradient of prior
+grad_prior = Gamma*laplacian*np.divide(G_t, G_h0)
+
+#---------------------------------------------------------------------------------#
+
+ITERATION_TIME = 10000
+MAX_ITER = 1000
+
+# MSE, PSNR Values
+Alpha = 0.1
+Beta = 0.001
+init_alpha = Alpha
+alpha_val = []
+mse_val = []
+psnr_val = []
+
+for i in tqdm(range(0, ITERATION_TIME)):
+
+    # start the gradient descent
+    for counter in range(0, MAX_ITER):
+        
+        updated_laplacian = cv2.Laplacian(I_h, -1)
+        
+        I_dt = cv2.resize(I_h, (height//4, width//4), interpolation = cv2.INTER_LINEAR)
+        difference = I_dt - I_l
+
+        # Get the prior and update gradient
+        prior = Beta*(updated_laplacian - grad_prior)
+        updated_grad = cv2.resize(difference, (height, width)) - prior
+        
+        # Update the value
+        I_h = I_h - Alpha*updated_grad
+
+    mse = MSE(I_gt, I_h, width, height)
+    psnr = PSNR(I_gt, mse)
+    mse_val.append(mse)
+    psnr_val.append(psnr)
+    alpha_val.append(Alpha)
+    Alpha += 0.00024
+
+#---------------------------------------------------------------------------------#
+
+# # Plotting part
+# plt.figure(figsize = (15, 10))
+# plt.plot(alpha_val, mse_val, color = 'black')
+# plt.title("Tendency between MSE and step size (at 1000 iteration)", fontsize = 20)
+# plt.axhline(500, color='black')
+# plt.axvline(init_alpha, color='black')
+# plt.xlabel('Step size value', fontsize = 17)
+# plt.ylabel('MSE value', fontsize = 17)
+
+# plt.savefig("Problem1_MSE.png", dpi=300)
+
+# plt.figure(figsize = (15, 10))
+# plt.plot(alpha_val, psnr_val, color = 'black')
+# plt.title("Tendency between PSNR and step size (at 1000 iteration)", fontsize = 20)
+# plt.axhline(30, color='black')
+# plt.axvline(init_alpha, color='black')
+# plt.xlabel('Step size value', fontsize = 17)
+# plt.ylabel('MSE value', fontsize = 17)
+
+# plt.savefig("Problem1_PSNR.png", dpi=300)
+
+np.save("/home/Computer_Vision_PA1/Analysis/alpha_val_2.npy", alpha_val)
+np.save("/home/Computer_Vision_PA1/Analysis/mse_val_2.npy", mse_val)
+np.save("/home/Computer_Vision_PA1/Analysis/psnr_val_2.npy", psnr_val)
